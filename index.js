2adbbb1b1a67fcbe7651ed643fd592071dc7620d
MalcolmMcConaghy <mcconaghym@live.co.uk>
2023-07-17 15:15:32 +0100

    organizations CRUD

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const port = 3000
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
 
-const db = require('./queries')
+const db = require("./queries");
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
-)
+);
 
-app.get('/', db.getUsers)
+app.get("/api/v1/organizations", db.getOrganizations);
+app.get("/api/v1/organizations/:id", db.getOrganizationsById);
+app.post("/api/v1/organizations", db.createOrganization);
+app.put("/api/v1/organizations/:id", db.updateOrganization);
+app.delete("/api/v1/organizations/:id");
 
 app.listen(port, () => {
-    console.log(`App running on localhost:${port}`)
-})
+  console.log(`App running on localhost:${port}`);
+});
--- queries.js
+++ queries.js
@@ -1,23 +1,72 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Pool = require('pg').Pool
+const Pool = require("pg").Pool;
 const pool = new Pool({
-    user: process.env.DOCKER_USER,
-    host: 'localhost',
-    database:  process.env.DOCKER_DB,
-    password: process.env.DOCKER_PASSWORD,
-    port: 5432
-})
+  user: process.env.DOCKER_USER,
+  host: "localhost",
+  database: process.env.DOCKER_DB,
+  password: process.env.DOCKER_PASSWORD,
+  port: 5432,
+});
 
-const getUsers = (req, res) => {
-    pool.query('SELECT * FROM users ORDER BY id ASC', (err, results) => {
-        if (err) {
-            throw err
-        }
-        res.status(200).json(results.rows)    
-    })
-}
+const CREATE_ORGANIZATIONS_TABLE_IF_NOT_EXISTS_QUERY =
+  "CREATE TABLE IF NOT EXISTS organizations (id SERIAL PRIMARY KEY, name VARCHAR(50) NOT NULL)";
+
+const getOrganizations = async (req, res) => {
+  await pool.query(CREATE_ORGANIZATIONS_TABLE_IF_NOT_EXISTS_QUERY);
+  pool.query("SELECT * from organizations", (err, results) => {
+    if (err) throw err;
+    res.status(200).json({ data: results.rows });
+  });
+};
+
+const getOrganizationsById = async (req, res) => {
+  const id = parseInt(req.params.id);
+
+  pool.query(
+    "SELECT * FROM organizations WHERE id = $1",
+    [id],
+    (err, results) => {
+      if (err) throw err;
+      res.status(200).json({ data: results.rows });
+    }
+  );
+};
+
+const createOrganization = async (req, res) => {
+  const { name } = req.body;
+  await pool.query(CREATE_ORGANIZATIONS_TABLE_IF_NOT_EXISTS_QUERY);
+  pool.query(
+    "INSERT INTO organizations (name) VALUES ($1) RETURNING *",
+    [name],
+    (err, results) => {
+      if (err) throw err;
+      res
+        .status(201)
+        .json({ data: `Organization added with ID: ${results.rows[0].id}` });
+    }
+  );
+};
+
+const updateOrganization = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { name } = req.body;
+
+  pool.query(
+    "UPDATE organizations SET name = $1 WHERE id = $2",
+    [name, id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).json({ data: `Organization modified with ID: ${id}` });
+    }
+  );
+};
 
 module.exports = {
-    getUsers
-}
+  getOrganizations,
+  getOrganizationsById,
+  createOrganization,
+  updateOrganization,
+};
